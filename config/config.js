723cdc642999efccb67fb95d60815ac3b10e149f
wuriyanto48 <wuriyanto48@yahoo.co.id>
2016-08-15 12:26:28 +0700

    azuread config

--- config/config.js
+++ config/config.js
@@ -8,4 +8,16 @@ config.userCollections = "userCollections";
 
 config.secret = 'bhinnekaIsGreat';
 
+// azure AD config
+config.azuread = {
+      clientID: 'your client ID',
+     audience: 'your application URL',
+  // example: https://login.microsoftonline.com/common/.well-known/openid-configuration
+     identityMetadata: 'https://login.microsoftonline.com/<your tenant id>/.well-known/openid-configuration',
+     validateIssuer: true, // if you have validation on, you cannot have users from multiple tenants sign in to your server
+     passReqToCallback: false,
+     loggingLevel: 'info' // valid are 'info', 'warn', 'error'.
+
+};
+
 module.exports = config;
