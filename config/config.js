2a09710ac15502d7e90578ae2103cb66fd45a581
Wuriyanto Musobar <wuriyanto48@yahoo.co.id>
2017-01-17 09:41:27 +0700

    Update config.js
    remove auth

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "https://test-client-credential.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "0nLil8zRVEPZn4oMv9exJbtG0JOApJoTFhHf2pcBnMAFKdQ1pvpDmvuvLvc2SgmQayxTIjdI8RAwsBUmYfIZ3g==";
+config.host = process.env.HOST || "";
+config.authKey = process.env.AUTH_KEY || "";
 config.databaseId = "TestClientCredential";
 config.productCollections = "productCollections";
 config.userCollections = "userCollections";
